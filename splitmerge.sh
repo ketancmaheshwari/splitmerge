68310204612788555bfde9c135b7f725a0316b32
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2023-02-20 09:23:09 -0500

    first version

--- splitmerge.sh
+++ splitmerge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail 
+
 SRC_AREA="/tmp/splitmerge/files"
 SPLIT_AREA="/tmp/splitmerge/splits"
 DONE_AREA="/tmp/splitmerge/done"
@@ -8,25 +10,44 @@ DBFILES="/tmp/splitmerge/dbfiles.txt"
 DBSPLITS="/tmp/splitmerge/dbsplits.txt"
 
 SIZE_THRESH=1024 #MB
+CHUNK_SIZE="10000K"
+
+# Generate dirs
+mkdir -p $SRC_AREA $SPLIT_AREA $DONE_AREA
 
 # Generate db files
 touch $DBFILES
 touch $DBSPLITS
 
-
-while true
-do
-	# find the largest file name and size
-
-	# if the size is greater than SIZE_THRESH
-	# generate a unique key based on the file checksum
-	# check if the checksummed pk does not already exist in the db
-	# if not, add the file entry to the db
-	# perform the split
-	# move the split parts to DEST_AREA
-	# Update the database
+if [ "$(find $SRC_AREA -type f|wc -l)" -lt 20 ]
+then
+  # Generate Test data
+  ./genrandomfd.sh -d 1 -f 20 -n $SRC_AREA -b 30 -e 40 -s 1130 
+fi
 
 
-	sleep 10m
-done
+#while true
+#do
+## find the files that are greater in size than the threshold and pick first
+
+file_to_split=$(find $SRC_AREA -type f -size +${SIZE_THRESH}M | head -1)
+if [ -s $file_to_split ]
+then
+  file_name=$(basename ${file_to_split})
+  mkdir -p ${SPLIT_AREA}/${file_name}
+  split -b ${CHUNK_SIZE} -d $file_to_split && mv x* ${SPLIT_AREA}/${file_name}/
+fi
+
+## if the size is greater than SIZE_THRESH
+## generate a unique key based on the file checksum
+## check if the checksummed pk does not already exist in the db
+## if not, add the file entry to the db
+## perform the split
+## move the split parts to DEST_AREA
+## Update the database
+#
+	sleep 10s
+#done
+
+echo "Done."
 
